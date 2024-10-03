f493467a775175ed7973287778870987d8ee2a69
noarkhh <jakub.pryc@gmail.com>
2024-10-03 14:41:52 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.RTSP.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @github_url "https://github.com/gBillal/membrane_rtsp_plugin"
 
   def project do
